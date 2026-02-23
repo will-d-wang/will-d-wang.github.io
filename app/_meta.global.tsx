05d3364ec7285363c5943da5c9087f005b9b3e9c
Will D. Wang <will-d-wang@users.noreply.github.com>
2026-02-23 14:19:32 -0800

    Removed contact page.

--- app/_meta.global.tsx
+++ app/_meta.global.tsx
@@ -18,33 +18,11 @@ export default {
     title: "Documentation",
     items: DOCS_ITEMS,
   },
-  article: {
-    type: "page",
-    theme: {
-      toc: false,
-      typesetting: "article",
-    },
-  },
-  contact: {
-    type: "page",
-    theme: {
-      layout: "full",
-      toc: false,
-      timestamp: false,
-    },
-  },
-  nextraStarter: {
-    title: "Starter Templates",
-    type: "menu",
-    items: {
-      docs: {
-        title: "Docs Starter repo",
-        href: "https://github.com/phucbm/nextra-docs-starter",
-      },
-      blog: {
-        title: "Blog Starter repo",
-        href: "https://github.com/phucbm/nextra-blog-starter",
-      },
-    },
-  },
+  // article: {
+  //   type: "page",
+  //   theme: {
+  //     toc: false,
+  //     typesetting: "article",
+  //   },
+  // },
 };
